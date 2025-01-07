93561eb82638cb1e0f461020b0f67fa6b856311a
deepak <dlipkumar77@gmail.com>
2025-01-07 18:24:36 +0530

    AppBar & backgroundColor Added

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color.fromARGB(128, 48, 46, 200),
+            title: Text('First App')),
+        backgroundColor: Colors.deepPurpleAccent,
         body: Center(child: Text('Hello world!')),
       ),
     ),
