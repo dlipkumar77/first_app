2b1eef2724a5723f56eb57b5008666f74c4948fc
deepak <dlipkumar77@gmail.com>
2025-01-07 17:35:48 +0530

    Scaffold Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Hello world!'),
-  ));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Hello world!')),
+      ),
+    ),
+  );
 }
