f93bda9f63d797513e15022b09dc3b19c249276a
deepak <dlipkumar77@gmail.com>
2025-01-08 12:17:23 +0530

    Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,28 @@ void main() {
             backgroundColor: Color.fromARGB(255, 12, 0, 40),
             title: Text('First App',
                 style: TextStyle(fontSize: 24, color: Colors.white))),
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 26, 2, 80),
-                  Color.fromARGB(255, 45, 7, 98),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: Center(
-                child: Text('Hello world!',
-                    style: TextStyle(fontSize: 28, color: Colors.white)))),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+            child: Text('Hello world!',
+                style: TextStyle(fontSize: 28, color: Colors.white))));
+  }
 }
