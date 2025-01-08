1c6eb92d20f2a4f7774196cb0938fa78e7feb37d
deepak <dlipkumar77@gmail.com>
2025-01-08 16:33:55 +0530

    Color Arguments Passing

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,19 +9,24 @@ const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   //const GradientContainer({Key? key}) : super(key: key);
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           //decoration: BoxDecoration(
           gradient: LinearGradient(
+            colors: [color1, color2],
+            /*
             colors: [
               //colors: const [
               // const keyword is before boxDecoration now colors because alignment is not const
               Color.fromARGB(255, 26, 2, 80),
               Color.fromARGB(255, 45, 7, 98),
             ],
+            */
             begin: starAlignment,
             end: endAlignment,
           ),
@@ -29,3 +34,34 @@ class GradientContainer extends StatelessWidget {
         child: Center(child: StyledText('Hello world!')));
   }
 }
+
+/*
+//Type one color Added to GradientContainer
+
+class GradientContainer extends StatelessWidget {
+  //const GradientContainer({Key? key}) : super(key: key);
+  const GradientContainer({super.key,required this.colors});
+  final List<Color> colors;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          //decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            /*
+            colors: [
+              //colors: const [
+              // const keyword is before boxDecoration now colors because alignment is not const
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98),
+            ],
+            */
+            begin: starAlignment,
+            end: endAlignment,
+          ),
+        ),
+        child: Center(child: StyledText('Hello world!')));
+  }
+}
+*/
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ void main() {
             backgroundColor: Color.fromARGB(255, 12, 0, 40),
             title: Text('First App',
                 style: TextStyle(fontSize: 24, color: Colors.white))),
-        body: GradientContainer(),
+        body: GradientContainer(const Color.fromARGB(255, 33, 5, 107),
+            const Color.fromARGB(255, 68, 21, 147)),
       ),
     ),
   );
