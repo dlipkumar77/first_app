fc78088299b5bda98c666f58a0efd369f2e5cb67
deepak <dlipkumar77@gmail.com>
2025-01-08 13:55:13 +0530

    Variables & Arguments passing values through parameters

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,12 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const starAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+//var starAlignment = Alignment.topLeft;
+//var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   //const GradientContainer({Key? key}) : super(key: key);
   const GradientContainer({super.key});
@@ -8,15 +14,18 @@ class GradientContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         decoration: const BoxDecoration(
+          //decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
+              //colors: const [
+              // const keyword is before boxDecoration now colors because alignment is not const
               Color.fromARGB(255, 26, 2, 80),
               Color.fromARGB(255, 45, 7, 98),
             ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+            begin: starAlignment,
+            end: endAlignment,
           ),
         ),
-        child: Center(child: StyledText()));
+        child: Center(child: StyledText('Hello world!')));
   }
 }
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.text, {super.key});
+  //const StyledText(String text,{super.key});
+  //const StyledText(String text,{super.key}) : outputText = text;
+  //String outputText;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Text('Hello world!',
-        style: TextStyle(fontSize: 28, color: Colors.white));
+    return Text(text,
+        style: const TextStyle(fontSize: 28, color: Colors.white));
   }
 }
